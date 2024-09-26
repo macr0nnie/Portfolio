f73f8b5386f89ec201f24d68ab84bae89c2c0eb2
macr0nnie <macr0nnieasmr@gmail.com>
2024-09-26 15:29:16 -0400

    addd
    asdfasdd

---
+++ PortfolioWebsite/scripts.js
@@ -0,0 +1,7 @@
+import { Application } from '@splinetool/runtime';
+
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
+app.load('https://prod.spline.design/M-2ESSn60ZHgdK0p/scene.splinecode');
+
+
